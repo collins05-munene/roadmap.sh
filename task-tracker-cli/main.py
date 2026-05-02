e14ef7d88958e56c9bbbe112477e714fdc147ea6
cybermun3n3 <collinsgitonga296@gmail.com>
2026-05-02 10:03:22 +0300

    task-tracker-cli(update)

--- task-tracker-cli/main.py
+++ task-tracker-cli/main.py
@@ -29,6 +29,35 @@ class Task:
 
         return (f"Task added successfuly (ID: {data['id']})")
     
+    def update(self, task_id, new_description=None, new_status=None):
+        try:
+            with open(filename, 'r')as file:
+                tasks = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return "No tasks found"
+        
+        found = False
+
+        for task in tasks:
+            if task["id"] == task_id:
+                if new_description:
+                    task['description'] = new_description
+                if new_status:
+                    task["status"] = new_status
+
+                task["updatedAt"] = datetime.now().isoformat()
+                found = True
+                break
+
+        if not found:
+            return f"Task with ID {task_id} not found."
+        
+        with open(filename, 'w') as file:
+            json.dump(tasks, file, indent=4)
+
+        return f"Tasks {task_id} updated successfully"
+        
 
 task = Task()
-print(task.add("Learn Django", "Todo"))
+
+print(task.update("5e29eeae-2805-471b-8c22-0b82c0c6e51c", None, "Done"))
